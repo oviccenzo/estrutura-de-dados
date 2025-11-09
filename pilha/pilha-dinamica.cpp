f1fc5197f3f8d692b1f6325dbb25b679d9ae9f04
viccenzo <viccenzo.oliveira@gmail.com>
2025-11-09 10:13:42 -0300

    Initial commit

--- pilha/pilha-dinamica.cpp
+++ pilha/pilha-dinamica.cpp
@@ -1,6 +1,9 @@
 //
 // Created by Robert L Resende on 09/11/25.
 //
+//Pilha dinamica encadeado
+//Implementação baseada em lista simplesmente ligada
 #include "cstdio"
 #include "cstdlib"
 
+//Definição da estrutura de um nó
