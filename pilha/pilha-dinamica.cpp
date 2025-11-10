bcc803f3947c309c436c62486dd1f52bb6ef5250
viccenzo <viccenzo.oliveira@gmail.com>
2025-11-09 22:33:44 -0300

    Initial commit

--- pilha/pilha-dinamica.cpp
+++ pilha/pilha-dinamica.cpp
@@ -7,3 +7,32 @@
 #include "cstdlib"
 
 //Definição da estrutura de um nó
+typedef struct Node{
+    int data; //dados
+    struct Node* proximo;//Ponteiro para o proximo nó
+} Node;
+
+//Estrutura para a pilha
+typedef struct Stack{
+    Node *parar; //ponteiro para o topo da pilha
+} pilha;
+
+//Função para inicializar a pilha
+void initializeStack(Stack* pilha){
+    pilha -> parar = NULL;
+}
+
+//Função para verificar se a pilha está vazia
+int isEmpty(Stack* pilha){
+    return (pilha -> parar == NULL);
+}
+
+//Função para criar um novo nó
+Node* createNode(int data){
+    Node* newNode = (Node*) malloc(sizeof (Node));
+    newNode -> data = data;
+    newNode -> proximo = NULL;
+    return newNode;
+}
+
+//Função para empilhar(inserir no topo da pilha)
