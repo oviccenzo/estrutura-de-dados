859194b0d475378ee6b9fbf0e340aedf3bb737ba
viccenzo <viccenzo.oliveira@gmail.com>
2025-11-13 22:37:53 -0300

    Initial commit

---
+++ fila/fila-circula-estatica.cpp
@@ -0,0 +1,88 @@
+//
+// Created by Robert L Resende on 05/11/25.
+//
+//implementação de fila circular estatica em vetor
+#include "cstdio"
+#include "cstdlib"
+#define Max 100 //tamanho da fila
+
+//estrutura da fila
+typedef struct {
+    int data[Max];
+    int front;
+    int rear;
+    int size;
+}Queue;
+
+//inicializa a fila
+void initializeQueue(Queue *q){
+    q -> front = 0;
+    q -> rear = 0;
+    q -> size = 0;
+}
+
+//verifica se a fila esta cheia
+int isFull(Queue *q){
+    return (q -> size == Max);
+}
+
+//Verifica se a fila esta vazia
+int isEmpty(Queue *q){
+    return (q -> size == 0);
+}
+
+//Verifica se a fila está vazia
+int Enqueue(Queue *q, int valor){
+    if(isFull(q)){
+        printf("Fila esta cheia. Nao e possivel inseirir na fila: %d\n", valor);
+        return 0;
+    }
+
+    q -> data[q -> rear] = valor;
+    q -> rear = (q -> rear + 1) % Max;
+    q -> size++;
+    return 1;
+}
+
+//Remove um valor na fila
+int Dequeue(Queue *q, int *valor){
+    if(isEmpty(q)){
+        printf("Fila esta cheia. Nao e possivel remover da fila!\n");
+        return 0;
+    }
+
+    *valor = q -> data[q -> front];
+    q -> front = (q -> front + 1) % Max ;
+    q -> size--;
+    return 1;
+}
+
+//Retorna o valor da cabeça da fila sem remover
+int front(Queue *q, int *valor){
+    if(isEmpty(q)){
+        printf("Fila esta vazia. Sem elemento na cabeca da fila!.\n");
+        return 0;
+    }
+    *valor = q -> data[q -> front];
+    return 1;
+}
+
+//Exibe todos os elementos da fila
+void displayQueue(Queue *q){
+    int index;
+
+    if(isEmpty(q)){
+        printf("Fila esta vazia\n");
+        return;
+    }
+    printf("Elementos na fila: ");
+    for (int i = 0; i < q -> size; ++i) {
+        index = (q -> front + 1) % Max;
+        printf("%d ", q -> data[index]);
+    }
+    printf("\n");
+}
+
+int main(){
+
+}
---
+++ pilha/pilha-dinamica-1.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Robert L Resende on 13/11/25.
+//
