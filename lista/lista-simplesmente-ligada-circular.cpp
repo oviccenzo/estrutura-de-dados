5fc86cc0313a3208c214f3f0d3dff20d2a7fd874
viccenzo <viccenzo.oliveira@gmail.com>
2025-11-15 17:07:41 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lista/lista-circular-ligada.cpp

--- lista/lista-simplesmente-ligada-circular.cpp
+++ lista/lista-simplesmente-ligada-circular.cpp
@@ -1,3 +1,30 @@
 //
-// Created by Robert L Resende on 05/11/25.
+// Created by Robert L Resende on 15/11/25.
 //
+#include <cstdio>
+#include <cstdlib>
+
+//Estrutura do nó da lista circular
+typedef struct Node{
+    int data;
+    struct Node* proximo;
+}Node;
+
+//Estrutura da lista circular
+typedef struct{
+    Node* head;
+}CircularLinkedList;
+
+//Função para inicializar a lista
+Node* createNode(int data){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if(!newNode){
+        printf("Erro de alocacao na memoria\n");
+        return NULL;
+    }
+    newNode -> data = data;
+    newNode -> next = newNode;//O nó aponta para si mesmo
+    return newNode;
+}
+
+int mian(){}
